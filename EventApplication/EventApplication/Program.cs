65aaf03ffdf4f9262abb163109fc0a7c40258f2b
Muhammad Fahad <siddiquiboy360@outlook.com>
2020-12-17 22:32:23 +0500

    new

---
+++ EventApplication/EventApplication/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myEvent me = new myEvent();
+
+            while (true)
+            {
+                ConsoleKeyInfo c = Console.ReadKey();
+
+                if (c.Key == ConsoleKey.W)
+                {
+                    me.RaiseEvent();
+                    break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EventApplication/EventApplication/myEvent.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventApplication
+{
+    //Step 1
+    public delegate void PrintDelegate();
+    class myEvent
+    {
+        //step 2
+        event PrintDelegate print;
+
+        //step 3
+        void show()
+        {
+            Console.WriteLine("Print Event has been raised.");
+            Console.WriteLine("This is a handler Method.");
+        }
+
+        //Creating a Constructor in which we assign event its handler
+        public myEvent()
+        {
+            print += show;
+        }
+
+        public void RaiseEvent()
+        {
+            //Raising the event named print
+            print();
+        }
+    }
+}
